0d18972012cca0e83941e6e638ed27809e6d1027
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-18 23:50:23 +0300

    Initial Commit

---
+++ server/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+});
