10a1fc721bff10252ae6b57a35ce12bddf35eada
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-19 23:16:03 +0300

    Server <> Adding Get Comments API

--- server/app/Http/Controllers/RecipeController.php
+++ server/app/Http/Controllers/RecipeController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use App\Models\Recipe;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class RecipeController extends Controller
 {
-    public function getRecipes(Request $request)
+    public function getRecipes()
     {
         $recipes = Recipe::all();
         foreach($recipes as $recipe){
@@ -18,10 +19,19 @@ class RecipeController extends Controller
             $recipe->cuisine;
             unset($recipe->likes);
             unset($recipe->comments);
-
         }
-        return response()->json(['message' => $recipes]);
-    
-}
+        return response()->json(['recipes' => $recipes]);
+    }
+
+    public function getComments(Request $request){
+        $recipe = Recipe::where('id', $request->id)->first();
+        foreach($recipe->comments as $comment){
+            $comment -> commenter = User::where('id', $comment->user_id)->pluck('name')->first();
+            unset($comment->user_id);
+            unset($comment->id);
+            unset($comment->recipe_id);
+        }
+        return response()->json(['comments' => $recipe->comments]);
+    }
 }
 
--- server/routes/api.php
+++ server/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get("recipes", [RecipeController::class, "getRecipes"]);
+Route::post("comments", [RecipeController::class, "getComments"]);
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
