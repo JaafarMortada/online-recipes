ec02cb2d76d56814292d0e3459b92d8e933feeaa
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-19 15:28:51 +0300

    Client <> Adding Base Recipe Card

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import RecipeCard from './components/recipeCard';
+import MySideBar from './components/sidebar';
 
 function App() {
   return (
+    <>
     <div className="App">
-      
+      <MySideBar/>
+      <div className='cards-container'>
+        <RecipeCard/>
+      </div>
     </div>
+    </>
   );
 }
 
