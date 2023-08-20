b44028c161a5ad60681ec9c822812bd10690edfe
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-20 15:17:11 +0300

    Client <> Adding Post Comment Request

--- client/src/components/modal/recipeInfoModalContent/index.jsx
+++ client/src/components/modal/recipeInfoModalContent/index.jsx
@@ -8,6 +8,35 @@ import { useEffect, useState } from 'react';
 const RecipeInfoModalContent = ( { data } ) => {
 
     const [comments, setComments] = useState([])
+    const [newComment, setNewComment] = useState({ 
+        comment: "",
+        recipe_id: data.id
+    })
+
+    const handleDataChange = (e)=>{
+        setNewComment({...newComment, [e.target.name]: e.target.value})
+    }
+
+    const commentingHandler = async () => {
+        try {
+            const response = await sendRequest({
+                method: "POST",
+                route: "/api/post_comment",
+                body: newComment,
+            });
+            if(response.status === "success"){
+                setComments([...comments, newComment.comment])
+                setNewComment({
+                    comment: "",
+                    recipe_id: data.id
+                })
+            } else {
+                console.log('failed')
+            }
+        } catch (error) {
+            console.log(error)
+        }}
+
     useEffect(() => {
         const getCommentsHandler = async () => {
             try {
@@ -21,7 +50,7 @@ const RecipeInfoModalContent = ( { data } ) => {
             }
         };
         getCommentsHandler()
-    }, [])
+    }, [comments])
 
     return (
         <div className='recipe-modal-content'>
@@ -31,8 +60,17 @@ const RecipeInfoModalContent = ( { data } ) => {
             </div>
             <div className='recipe-modal-content-right'>
                 <div className='send-comment'>
-                    <TextInput placeholder={'write a comment!'} />
-                    <MyButton label={< AiOutlineSend />} styles={{ marginTop: "10px", width: "40px" }} />
+                    <TextInput 
+                        name={"comment"}
+                        value={newComment.comment}
+                        placeholder={'write a comment!'}
+                        onChange={handleDataChange}
+                    />
+                    <MyButton 
+                        label={< AiOutlineSend />} 
+                        styles={{ marginTop: "10px", width: "40px" }}
+                        onClick={commentingHandler}
+                        />
                 </div>
                 <div className='comments-container'>
                     {
