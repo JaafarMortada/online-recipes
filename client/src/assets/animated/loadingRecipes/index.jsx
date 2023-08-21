e2070697320e99a0e6b9f40569ba15077dd18bb0
Jaafar Mortada <jaafar1899@outlook.com>
2023-08-21 03:58:03 +0300

    Client <> Adding Create Recipe Request

--- client/src/assets/animated/loadingRecipes/index.jsx
+++ client/src/assets/animated/loadingRecipes/index.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import Lottie from "lottie-react";
 import loadingAnimation from "./animation_lljbre1t.json"
 
-const LoadingAnimation = () => <Lottie animationData={loadingAnimation} loop={true} />;
+const LoadingAnimation = () => <Lottie animationData={loadingAnimation} loop={true} style={{height:"105vh"}}/>;
 
 export default LoadingAnimation;
--- client/src/components/modal/addRecipeModalContent/index.jsx
+++ client/src/components/modal/addRecipeModalContent/index.jsx
@@ -1,9 +1,21 @@
 import MyButton from "../../button";
 import TextInput from "../../textInput/Index";
 import { useState } from "react";
+import { sendRequest } from '../../../config/request';
+
 import "./styles.css"
 
 const AddRecipeFromModal = () => {
+
+    const [files, setFiles] = useState([])
+
+    const handleFile = (e) => {
+        const newFiles = []
+        for(let i = 0; i < e.target.files.length; i++){
+            newFiles.push(e.target.files[i])
+        }
+        setFiles(newFiles)
+    } // https://stackoverflow.com/questions/70473346/how-do-i-get-mutliple-files-in-usestate-in-reactnext-js
 
     const [data, setData] = useState({
         name:"",
@@ -32,8 +44,49 @@ const AddRecipeFromModal = () => {
         })
     }
 
-    const handleSubmitClick = () => {
-        console.log({name: data.name, cuisine: data.cuisine, ingredients:JSON.stringify(ingredients)})
+    const handleSubmitClick = async () => {
+
+        const create_recipe_button = document.getElementById("create-recipe-button")
+        create_recipe_button.innerHTML = 'Sharing...'
+        const formData = new FormData();
+        formData.append('name', data.name);
+        formData.append('cuisine', data.cuisine);
+        formData.append('ingredients', JSON.stringify(ingredients));
+        for (const file of files) {
+            formData.append('image_url[]', file);
+        }
+
+        try {
+            const response = await sendRequest({
+                method: "POST",
+                route: "/api/create_recipe",
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+                body: formData,
+            });
+            if(response.status === "success"){
+                create_recipe_button.innerHTML = 'success'
+                setFiles([])
+                setData({
+                    name: '',
+                    cuisine: '',
+                    ingredient: '',
+                    amount: '',
+                })
+                setIngredients([])
+            } else {
+                create_recipe_button.innerHTML = 'Failed'
+                create_recipe_button.style.backgroundColor = 'rgb(255, 109, 109)'
+                setTimeout(() => { 
+                    create_recipe_button.innerHTML = 'Share!' 
+                    create_recipe_button.style.backgroundColor = 'rgb(247, 129, 91)'
+                }, 3000)
+            }
+        } catch (error) {
+            console.log(error)
+            create_recipe_button.innerHTML = 'Try Again' 
+        }
     }
     
     return ( 
@@ -71,6 +124,7 @@ const AddRecipeFromModal = () => {
                 label={"Images:"}
                 type={"file"}
                 multiple={true}
+                onChange={handleFile}
             />
             <div className={"add-recipe-modal-buttons-container"}>
                 <MyButton 
@@ -79,6 +133,7 @@ const AddRecipeFromModal = () => {
                     onClick={handleAddClick}
                 />
                 <MyButton 
+                    id={"create-recipe-button"}
                     label={"Share!"} 
                     styles={{width:"150px", padding:"10px 0 10px"}}
                     onClick={handleSubmitClick}
